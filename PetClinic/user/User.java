98bf3c8cc2993bfd744169d485370e176ceb238d
Rojemmarc Gutierrez <gutierrezrojemmarc4@gmail.com>
2026-04-27 10:43:13 +0800

    Add files via upload

---
+++ PetClinic/user/User.java
@@ -0,0 +1,22 @@
+package model.user;
+
+public abstract class User {
+    protected String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[name=" + name + "]";
+    }
+}
