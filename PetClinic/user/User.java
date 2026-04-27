53b789ce09e2b13b371e8c85bfad369d2c4d19e9
Rojemmarc Gutierrez <gutierrezrojemmarc4@gmail.com>
2026-04-27 10:46:05 +0800

    Update User.java

--- PetClinic/user/User.java
+++ PetClinic/user/User.java
@@ -1,22 +1,42 @@
 package model.user;
 
 public abstract class User {
-    protected String name;
+    private static int idCounter = 1000;
 
-    public User(String name) {
-        this.name = name;
+    private final int userId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public User(String name, String email, String phone, String address) {
+        this.userId  = idCounter++;
+        this.name    = name;
+        this.email   = email;
+        this.phone   = phone;
+        this.address = address;
     }
 
-    public String getName() {
-        return name;
+    public abstract String getRole();
+
+    public void displayInfo() {
+        System.out.println("-----------------------------");
+        System.out.println("Role    : " + getRole());
+        System.out.println("ID      : " + userId);
+        System.out.println("Name    : " + name);
+        System.out.println("Email   : " + email);
+        System.out.println("Phone   : " + phone);
+        System.out.println("Address : " + address);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public int getUserId()           { return userId; }
+    public String getName()          { return name; }
+    public String getEmail()         { return email; }
+    public String getPhone()         { return phone; }
+    public String getAddress()       { return address; }
+    public void setName(String n)    { this.name = n; }
+    public void setEmail(String e)   { this.email = e; }
+    public void setPhone(String p)   { this.phone = p; }
+    public void setAddress(String a) { this.address = a; }
 
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "[name=" + name + "]";
-    }
 }
