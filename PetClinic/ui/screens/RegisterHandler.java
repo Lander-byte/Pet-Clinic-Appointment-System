9da4b59cf02ef3d4e711cdbbc31e4e4f3c6e1c6a
Rojemmarc Gutierrez <gutierrezrojemmarc4@gmail.com>
2026-05-13 22:36:36 +0800

    Delete PetClinic/ui/screens/RegisterHandler.java

--- PetClinic/ui/screens/RegisterHandler.java
+++
@@ -1,8 +0,0 @@
-package PetClinic.ui.screens;
-
-import PetClinic.ui.components.FloatingInput;
-
-@FunctionalInterface
-public interface RegisterHandler {
-    void register(FloatingInput username, FloatingInput email, FloatingInput password, FloatingInput confirm);
-}
