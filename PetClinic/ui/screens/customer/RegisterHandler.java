b4283a41e39a4e28a856a1f7cc964c9fbc57f6ec
Don <brandoncarl1231@gmail.com>
2026-05-14 17:07:56 +0800

    Add RegisterHandler interface for customer registration

---
+++ PetClinic/ui/screens/customer/RegisterHandler.java
@@ -0,0 +1,8 @@
+package ui.screens.customer;
+
+import ui.components.FloatingInput;
+
+@FunctionalInterface
+public interface RegisterHandler {
+    void register(FloatingInput var1, FloatingInput var2, FloatingInput var3, FloatingInput var4);
+}
